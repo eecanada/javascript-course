ff4f6dede8ace3dfb3c32518b1f8223e4d4f2ed5
Eder Canada <canada.eder@gmail.com>
2019-12-14 21:26:22 -0800

    data types

--- js-basics/index.js
+++ js-basics/index.js
@@ -1,1 +1,15 @@
-console.log('Hello World')
+/////////////////////////// DATA TYPES ///////////////////////
+
+// String 
+const firstName = 'John'
+const lastName = 'Smith'
+
+// Number
+let age = 28 
+
+// Boolean
+const fullAge = true 
+
+// Undefined 
+let job; 
+console.log(job)
