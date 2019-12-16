f16882fb53c75fefc4f5054429a8444e8f3db4ac
Eder Canada <canada.eder@gmail.com>
2019-12-15 17:09:45 -0800

    variable mutations

--- js-basics/index.js
+++ js-basics/index.js
@@ -18,15 +18,28 @@
 
 ////////////////Variable Mutation and Type Coercion///////////
 
-// Examples of Type Coercion, JS converts types from one to another as needed 
-let firstName = 'John'
-let age = 28
-console.log(firstName + " "+ age) // --> John 28
+// // Examples of Type Coercion, JS converts types from one to another as needed 
+// let firstName = 'John'
+// let age = 28
+// console.log(firstName + " "+ age) // --> John 28
 
 
-var job, isMarried 
-job = 'teacher'
-isMarried = false 
-console.log(firstName + ' is a ' + age + ' year old ' + job +'. Is he married? ' + isMarried )
+// var job, isMarried 
+// job = 'teacher'
+// isMarried = false 
+// console.log(firstName + ' is a ' + age + ' year old ' + job +'. Is he married? ' + isMarried )
 
+
+// // Examples of Variable Mutations 
+// age = 'twenty-eight'
+// job = 'driver'
+// alert(firstName + ' is a ' + age + ' year old ' + job +'. Is he married? ' + isMarried)
+
+// // here I am able to use prompt to display a prompt to answer and save the answer a variable lastName 
+// var lastName = prompt('what is his last name?')
+// console.log(firstName + ' ' + lastName)
+
+
+
+///////////////////////Basic Operators////////////////////////
 
