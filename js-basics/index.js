2deaa40d953129bc0403c736f055f8f4e5b70f65
Eder Canada <canada.eder@gmail.com>
2019-12-15 22:13:18 -0800

    operator precedene

--- js-basics/index.js
+++ js-basics/index.js
@@ -43,26 +43,66 @@
 
 ///////////////////////Basic Operators////////////////////////
 
-var now, yearJohn, yearMark
-now = 2019
-ageJohn = 28
-ageMark = 33
+// var now, yearJohn, yearMark
+// now = 2019
+// ageJohn = 28
+// ageMark = 33
 
-// Examples of Math Operator 
-yearJohn  = now - ageJohn
-yearMark = now - ageMark
-console.log(yearJohn)
-console.log(now + 2)
-console.log(now * 2)
-console.log(now  / 10)
+// // Examples of Math Operator 
+// yearJohn  = now - ageJohn
+// yearMark = now - ageMark
+// console.log(yearJohn)
+// console.log(now + 2)
+// console.log(now * 2)
+// console.log(now  / 10)
 
-// Examples of Logical Operators 
-var johnOlder = ageJohn > ageMark 
-console.log(johnOlder)
+// // Examples of Logical Operators 
+// var johnOlder = ageJohn > ageMark 
+// console.log(johnOlder)
 
-// Example of typeof Operator 
-console.log(typeof(johnOlder)) //--> boolean
-console.log(typeof(ageJohn)) //--> number
-console.log(typeof('JS is fun'))//--> String
-var x
-console.log(typeof(x))//--> undefined
+// // Example of typeof Operator 
+// console.log(typeof(johnOlder)) //--> boolean
+// console.log(typeof(ageJohn)) //--> number
+// console.log(typeof('JS is fun'))//--> String
+// var x
+// console.log(typeof(x))//--> undefined
+
+
+
+
+///////////////////Operator Precedence////////////////////////
+
+// var now = 2019
+// var yearJohn = 1989
+// var fullAge = 18
+
+// // Multiple Operators
+// var isFullAge = now - yearJohn >= fullAge
+// console.log(isFullAge)//--> true
+
+// // Grouping
+// var ageJohn = now - yearJohn
+// var ageMark = 35
+// var average = (ageJohn + ageMark) / 2
+// console.log(average)//--> 32.5
+
+// // Multiple Assignments 
+// var x,y 
+// x = y = (3 + 5) * 4 - 6
+// console.log(x) //--> 26
+
+// // More Operators 
+// x = x * 2
+// x *= 2
+
+// x = x + 10
+// x += 10
+
+// x = x / 2 
+// x /= 2
+
+// x = x + 1 
+// x ++
+
+// x = x -1
+// x --
