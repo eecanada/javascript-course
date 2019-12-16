5159a6aec094be66fabc5c8b0909e2c759eba8b3
Eder Canada <canada.eder@gmail.com>
2019-12-15 16:56:45 -0800

    type coercion

--- js-basics/index.js
+++ js-basics/index.js
@@ -1,15 +1,32 @@
 /////////////////////////// DATA TYPES ///////////////////////
 
-// String 
-const firstName = 'John'
-const lastName = 'Smith'
+// // String 
+// const firstName = 'John'
+// const lastName = 'Smith'
 
-// Number
-let age = 28 
+// // Number
+// let age = 28 
 
-// Boolean
-const fullAge = true 
+// // Boolean
+// const fullAge = true 
 
-// Undefined 
-let job; 
-console.log(job)
+// // Undefined 
+// let job; 
+// console.log(job)
+
+
+
+////////////////Variable Mutation and Type Coercion///////////
+
+// Examples of Type Coercion, JS converts types from one to another as needed 
+let firstName = 'John'
+let age = 28
+console.log(firstName + " "+ age) // --> John 28
+
+
+var job, isMarried 
+job = 'teacher'
+isMarried = false 
+console.log(firstName + ' is a ' + age + ' year old ' + job +'. Is he married? ' + isMarried )
+
+
