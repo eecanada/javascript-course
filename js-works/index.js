480f164f828ef87027f6729d7b73d9dfc162cc61
Eder Canada <canada.eder@gmail.com>
2019-12-21 19:27:03 -0800

    excution context

--- js-works/index.js
+++ js-works/index.js
@@ -4,4 +4,10 @@
 
 // Code is parsered by a parser ---> reads code line by line and sees if syntax is correct ----> if everything is correct then the parser produces a Abstract syntax tree ----> tree is translated into machine code ----> turns js code into code that can be excuted directly by a cpu proccessor ----> then code finally runs 
  
-//
+
+
+//////////Execution Context and the Execution Stack///////////
+
+// all JS codes need an enviroment to run in, and these enviroments are called execution context 
+
+// lastName === window.lastName, because lastName is a property of the window object
