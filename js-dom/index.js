adb6c81d96484713688e8041b7b0d4e737a36236
Eder Canada <canada.eder@gmail.com>
2019-12-25 23:01:57 -0800

    begin game structure

--- js-dom/index.js
+++ js-dom/index.js
@@ -3,3 +3,14 @@
 // DOM: structured representation of an HTML document, that I am going to use to webpages to scripts like JS
 
 // For each HTML box there is an object in the DOM that I access and interact with. The DOM and JS are NOT the same thing
+
+
+
+/*
+GAME RULES:
+- The game has 2 players, playing in rounds
+- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The first player to reach 100 points on GLOBAL score wins the game
+*/
