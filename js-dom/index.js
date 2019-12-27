9b8d0601a28b03df3edb72cfb8052b1403bc3a5f
Eder Canada <canada.eder@gmail.com>
2019-12-26 23:04:23 -0800

    dice display

--- js-dom/index.js
+++ js-dom/index.js
@@ -14,3 +14,19 @@ GAME RULES:
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
 */
+
+const scores, roundScores, activePlayer, dice
+
+score = [0,0]
+roundScore = 0
+activePlayer = 1
+
+dice = Math.floor(Math.random() * 6) + 1
+
+document.querySelector('#current-' + activePlayer).textContent = dice
+
+const x = document.querySelector('#score-0').textContent
+console.log(x)
+
+document,querySelector('.dice').style.display = 'none'
+
