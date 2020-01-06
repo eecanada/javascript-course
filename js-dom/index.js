2c3eca4b9c502b86d8cee8592de11fcadc1b4f1c
Eder Canada <canada.eder@gmail.com>
2020-01-06 11:49:13 -0800

    set state variable, game complete

--- js-dom/index.js
+++ js-dom/index.js
@@ -21,10 +21,11 @@ GAME RULES:
 // change CSS Style 
 
 ////////////////////////////////////////////////// create game variables
-let scores, roundScore, activePlayer
+let scores, roundScore, activePlayer, gamePlaying 
 scores = [0,0]
 roundScore = 0 // is equal to current-
 activePlayer = 0 // 0 first player, 1 second player 
+gamePlaying = true
 
 ////////////////////////////////////////////////// generate random number in my dice from 1 - 6
 // dice = Math.floor(Math.random() * 6) + 1 
@@ -60,9 +61,6 @@ document.querySelector('.dice').style.display = 'none'
 // Ternary operator
     // add,remove and toggel HTML classes
 
-
-
-
 // Both the global and current score are set to 0
 document.getElementById('score-0').textContent = '0'
 document.getElementById('score-1').textContent = '0'
@@ -81,52 +79,57 @@ document.getElementById('current-1').textContent = '0'
 //////////////////////////////////////////////////////////anonymous functions
 // can only be within the context 
 document.querySelector('.btn-roll').addEventListener('click', function(){
-  //////////////////////// set random number
-  const dice = Math.floor(Math.random() * 6) + 1 
-  ///////////////////// display random number as dice image
-  const diceDOM = document.querySelector('.dice')
-  diceDOM.style.display = 'block' 
-  //////////////////////////// change the image in <img> tag 
-  diceDOM.src =  `dice-${dice}.png`
-  /////////////////////////// update current score on the DOM or switch active player
-  if(dice !== 1){
-    // roundScore = roundScore + dice 
-    roundScore += dice
-    document.getElementById(`current-${activePlayer}`).textContent = roundScore 
-  } else {
-    // NEXT PLAYER
-    ////////////////////////switch players and set the roundScore which is current score to 0 
-    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0
-    // globalVariable roundScore  
-    roundScore = 0
-    /////////////////////// after a player gets a 1, the current score gets set to 0 
-    document.getElementById('current-0').textContent = '0'
-    document.getElementById('current-1').textContent = '0'
-    ////////////////////////////// add,remove and toggle HTML classes
-    // what toggle does is that it add the class if it is not there, and if it is there, then it is removed
-    document.querySelector('.player-0-panel').classList.toggle('active');
-    document.querySelector('.player-1-panel').classList.toggle('active')
-    document.querySelector('.dice').style.display = 'none'
+  if(gamePlaying){
+    //////////////////////// set random number
+    const dice = Math.floor(Math.random() * 6) + 1 
+    ///////////////////// display random number as dice image
+    const diceDOM = document.querySelector('.dice')
+    diceDOM.style.display = 'block' 
+    //////////////////////////// change the image in <img> tag 
+    diceDOM.src =  `dice-${dice}.png`
+    /////////////////////////// update current score on the DOM or switch active player
+    if(dice !== 1){
+      // roundScore = roundScore + dice 
+      roundScore += dice
+      document.getElementById(`current-${activePlayer}`).textContent = roundScore 
+    } else {
+      // NEXT PLAYER
+      ////////////////////////switch players and set the roundScore which is current score to 0 
+      activePlayer === 0 ? activePlayer = 1 : activePlayer = 0
+      // globalVariable roundScore  
+      roundScore = 0
+      /////////////////////// after a player gets a 1, the current score gets set to 0 
+      document.getElementById('current-0').textContent = '0'
+      document.getElementById('current-1').textContent = '0'
+      ////////////////////////////// add,remove and toggle HTML classes
+      // what toggle does is that it add the class if it is not there, and if it is there, then it is removed
+      document.querySelector('.player-0-panel').classList.toggle('active');
+      document.querySelector('.player-1-panel').classList.toggle('active')
+      document.querySelector('.dice').style.display = 'none'
+    }
   }
+  
 })
 
 
 document.querySelector('.btn-hold').addEventListener('click', function(){
-  ///////////////// add current score to global score 
-  scores[activePlayer] += roundScore
- //////////////////// update UI of current player 
-  document.querySelector(`#score-${activePlayer}`).textContent = scores[activePlayer]
-  /////////////////// check if player won
-  if(scores[activePlayer] >= 10){
-    document.querySelector(`#name-${activePlayer}`).textContent = 'Winner!'
-    document.querySelector('.dice').style.display = 'none'
-    document.querySelector(`.player-${activePlayer}-panel`).classList.add('winner')
-    document.querySelector(`.player-${activePlayer}-panel`).classList.remove('active')
-
-  } else{
-    //////// Next Player 
-    nextPlayer()
-  }  
+  if(gamePlaying){
+    ///////////////// add current score to global score 
+    scores[activePlayer] += roundScore
+   //////////////////// update UI of current player 
+    document.querySelector(`#score-${activePlayer}`).textContent = scores[activePlayer]
+    /////////////////// check if player won
+    if(scores[activePlayer] >= 10){
+      document.querySelector(`#name-${activePlayer}`).textContent = 'Winner!'
+      document.querySelector('.dice').style.display = 'none'
+      document.querySelector(`.player-${activePlayer}-panel`).classList.add('winner')
+      document.querySelector(`.player-${activePlayer}-panel`).classList.remove('active')
+      gamePlaying = false 
+    } else{
+      //////// Next Player 
+      nextPlayer()
+    }  
+  }
 })
 
 // made this function because I am using the code in the fuction twice on both of my anonymous to let me move on to the next player
@@ -141,11 +144,12 @@ function nextPlayer (){
 }
 
 document.querySelector('.btn-new').addEventListener('click', init)
-
+// reset game 
 function init (){
   scores = [0,0]
   activePlayer = 0
   roundScore = 0
+  gamePlaying = true
   document.querySelector('.dice').style.display = 'none' 
   document.getElementById('score-0').textContent = '0'
   document.getElementById('score-1').textContent = '0'
@@ -159,5 +163,7 @@ function init (){
   document.querySelector('.player-0-panel').classList.remove('active')
   document.querySelector('.player-1-panel').classList.remove('active')
   document.querySelector('.player-0-panel').classList.add('active')
-
 }
+
+
+// state variable tells me the condition of a system and I need it when I want to remember something or the state of something and in this case it will be is out game playing or is out game not playing. 
