2958b3b2a3c2db7bb21f333eb0c48f0caf0c5f67
Eder Canada <canada.eder@gmail.com>
2020-01-08 21:26:16 -0800

    primitive objects lesson

--- js-object-functions/index.js
+++ js-object-functions/index.js
@@ -58,25 +58,58 @@
 
 //////////////////////////////////OBJECT.CREATE//////////////////////
 
-// The difference between object.create and function constructor, is that object.create builds an object that inherits directly from the one that I passed into the first arguement, while the with the function constructor the newly created object inherits from the constructor prototype property, so that object I made is an instance, but with object.create it inherits.
+// // The difference between object.create and function constructor, is that object.create builds an object that inherits directly from the one that I passed into the first arguement, while the with the function constructor the newly created object inherits from the constructor prototype property, so that object I made is an instance, but with object.create it inherits.
 
-const personProto = {
-  calculateAge: function(){
-    console.log(2020 - this.yearOfBirth)
-    },
-  lastName: 'Smith'
+// const personProto = {
+//   calculateAge: function(){
+//     console.log(2020 - this.yearOfBirth)
+//     },
+//   lastName: 'Smith'
+// }
+
+// const john = Object.create(personProto)
+// john.name = 'John'
+// john.yearOfBirth = 1990
+// john.job = 'Dentist'
+// console.log(john ) // --> logs the whole object
+
+// const jane = Object.create(personProto,{
+//   name: {value: 'Jane'},
+//   yearOfBirth: {value: 1973},
+//   job: {value: 'Teacher'}
+// })
+
+//////////////////////////////////PRIMITIVES VS OBJECTS//////////////////////
+// Primitive
+var a = 23
+var b = a
+a = 46
+// console.log(a,b) // -> 46,23
+
+
+
+// Objects
+var obj1 = {
+  name: 'eder',
+  age: 24
 }
 
-const john = Object.create(personProto)
-john.name = 'John'
-john.yearOfBirth = 1990
-john.job = 'Dentist'
-console.log(john ) // --> logs the whole object
+var obj2 = obj1
+obj1.age = 30
 
-const jane = Object.create(personProto,{
-  name: {value: 'Jane'},
-  yearOfBirth: {value: 1973},
-  job: {value: 'Teacher'}
-})
+// console.log(obj1,obj2)
 
+// Functions
+var age = 27
+var obj = {
+  name: 'mike',
+  city: 'LA',
+}
 
+function change (a,b){
+  a = 30
+  b.city = 'SF'
+}
+
+change(age, obj)
+console.log(age, obj.city) //---> 27, SF
