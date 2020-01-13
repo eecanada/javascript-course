c166288c65322b0062e7791b45274025b741c5f1
Eder Canada <canada.eder@gmail.com>
2020-01-13 12:10:38 -0800

    edit variable names on retirement function

--- js-object-functions/index.js
+++ js-object-functions/index.js
@@ -253,33 +253,45 @@
 
 //////////////////////////////////Closures//////////////////////////
 
-function retirement(retirementAge) {
-  var a = ' years left until retirement.';
-  return function(yearOfBirth) {
-      var age = 2016 - yearOfBirth;
-      console.log((retirementAge - age) + a);
-  }
-}
-var retirementUS = retirement(66);
-var retirementGermany = retirement(65);
-var retirementIceland = retirement(67);
-retirementGermany(1990);
-retirementUS(1990);
-retirementIceland(1990);
-//retirement(66)(1990);
+// so closure is when an inner function is able to access the variables of the outer function, even after the outer function has returned. 
 
 
-function interviewQuestion(job) {
-  return function(name) {
-      if (job === 'designer') {
-          console.log(name + ', can you please explain what UX design is?');
-      } else if (job === 'teacher') {
-          console.log('What subject do you teach, ' + name + '?');
-      } else {
-          console.log('Hello ' + name + ', what do you do?');
-      }
+function retirement(ageOfRetirement){
+  const sentence = 'years left of retirement.'
+  return function(birthYear){
+    const age = 2020 - birthYear
+    console.log(`${ageOfRetirement}` - `${age}` + ` ${sentence}`)
   }
 }
-interviewQuestion('teacher')('John');
+
+
+const retirementUnitedStates = retirement(65)
+retirementUnitedStates(1994)
+console.log(retirementUnitedStates) //--> 39 years left of retirement.
+
+
+const retirementFinland = retirement(60)(1990) 
+console.log(retirementFinland) // --> 30 years left of retirement.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
