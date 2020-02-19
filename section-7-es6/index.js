319a14a0b9332c17594f8f9cfa8cc1673aa6b6dc
Eder Canada <canada.eder@gmail.com>
2020-02-19 09:28:55 -0800

    destructuring

--- section-7-es6/index.js
+++ section-7-es6/index.js
@@ -1,4 +1,4 @@
-////////////////////////////////////////////////////////LET AND CONST////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////LET AND CONST////////////////////////////////////////////////////////////////
 
 //ES5 - function scope
 // var name5 = 'Jane Smith'
@@ -64,7 +64,7 @@
 
 
 
-//////////////////////////////////////////////////////////////////////STRINGS////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////STRINGS///////////////////////////////////////////////////////////////
 
 // let firstName = 'john'
 // let lastName = 'Smith'
@@ -101,8 +101,8 @@
 //  })
 //  console.log(ages5) //[ 26, 51, 34, 79 ]
 
- //ES6 - with map i have access to the current index, element and array itself
- // 1st Method - one arguement and one line of code 
+//  // ES6 - with map i have access to the current index, element and array itself
+//  //1st Method - one arguement and one line of code 
 //  let  ages6 = years.map (el => 2016 - el)
 //  console.log(ages6) //[ 26, 51, 34, 79 ]
 
@@ -124,7 +124,7 @@
 
 
 
-/////////////////////////////////////////////////////////////ARROW FUNCTIONS[THIS KEYWORD]//////////////////////////////////////////////////
+///////////////////////////////////////////////////////////ARROW FUNCTIONS[THIS KEYWORD]//////////////////////////////////////////////////
 
 // //ES5
 // var box5 = {
@@ -140,7 +140,7 @@
 // box5.clickMe() 
 
 
-//ES6 - arrrow functions share the this lexical of its surroundings, which is one level above it 
+// //ES6 - arrrow functions share the this lexical of its surroundings, which is one level above it 
 // const box6 = {
 //   color: 'green',
 //   position: 1,
@@ -155,10 +155,10 @@
 
 
 
-// function constructor 
-function Person (name){
-  this.name = name
-}
+// // function constructor 
+// function Person (name){
+//   this.name = name
+// }
 
 // //ES5
 // Person.prototype.myFriends5 = function (friends) {
@@ -169,15 +169,72 @@ function Person (name){
 //    console.log(arr) // 'undefined is friends with bob', wo ill not work because this is pointing to global object
 //   }
 
-  var friends = ['bob','jane','mark']
-  // new Person('John').myFriends5(friends)
+//   var friends = ['bob','jane','mark']
+//   // new Person('John').myFriends5(friends)
 
 
-//ES6 this fuctions does not have a this keyword
-Person.prototype.myFriends6 = function (friends) {
-  let arr = friends.map(el => `${this.name} is friends with ${el}`)
-  console.log(arr)
-  }
+// //ES6 this fuctions does not have a this keyword
+// Person.prototype.myFriends6 = function (friends) {
+//   let arr = friends.map(el => `${this.name} is friends with ${el}`)
+//   console.log(arr)
+//   }
 
-  // instance of Person objecy 
-  new Person('Eder').myFriends6(friends)
+//   // instance of Person objecy 
+//   new Person('Eder').myFriends6(friends)
+
+
+  
+
+///////////////////////////////////////////////////////////DESTRUCTURING//////////////////////////////////////////////////////////////////
+
+// //ES5
+// var john = ['John', 26]
+// var name = john[0]
+// var age = john[1]
+
+// //ES6 - destructuring for array use brackets 
+// const [name, age] = ['Eder', 26 ]
+// console.log(name) // eder
+// console.log(age) // 26
+
+// const [hobby, book] = ['traveling','tribes of mentor']
+// console.log(hobby) // traveling
+// console.log(book) // tribes of mentors 
+
+// //ES6 destructuring for objects use curly brackets, keys have to match 
+// const obj = {
+//   firstName: 'Eder',
+//   lastName: 'Canada'
+// }
+
+// const {firstName, lastName} = obj
+// console.log(firstName) // Eder
+// console.log(lastName) // Canada 
+
+// // This is to rename the key variable name, but it will still have the same value 
+// const{firstName: a, lastName: b} = obj
+// console.log(a) // Eder
+// console.log(b) // Canada
+
+
+// function calcAgeRetirement(year) {
+//    const age = new Date().getFullYear() - year
+//    return [age, 65 - age] // age is one variable and 65-age is another variable(retirement)
+//   }
+
+// const [age, retirement] = calcAgeRetirement(1990)
+// console.log(age, 'age') // 35
+// console.log(retirement, 'retirement') // 30
+
+
+// function vacationYear(year) {
+//   const yearsLeft = Math.abs(new Date().getFullYear() - year)
+//   return [yearsLeft]
+//   }
+
+// const [yearsLeft] = vacationYear(2021)
+// console.log(yearsLeft) // 1 
+
+
+
+///////////////////////////////////////////////////////////DESTRUCTURING//////////////////////////////////////////////////////////////////
