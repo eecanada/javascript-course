43a5f9b9f3509b20dea7fe981b718852f94d1898
Eder Canada <canada.eder@gmail.com>
2020-04-04 15:41:22 -0700

    add hoisting comments

--- section-3-js-works/index.js
+++ section-3-js-works/index.js
@@ -1,30 +1,30 @@
-////////////////////////////////////////////////////////How Code is Executed/////////////////////////
+////////////////////////////////////////////////////////How Code is Executed////////////////////////////////////
 
 // Host (browser) where JS is hosted has some sort of JS engine that takes my code and excutes it. So a JS engine is a program that excutes JS code. 
 
-// Code is parsered by a parser ---> reads code line by line and sees if syntax is correct ----> if everything is correct then the parser produces a Abstract syntax tree ----> tree is translated into machine code ----> turns js code into code that can be excuted directly by a cpu proccessor ----> then code finally runs 
+// In the code engine my code is parsered by a parser ---> reads code line by line and sees if syntax is correct ----> if everything is correct then the parser produces an Abstract syntax tree ----> tree is translated into machine code ----> turns js code into code that can be excuted directly by a cpu proccessor ----> then code finally runs 
  
 
 
-////////////////////////////////////Execution Context and the Execution Stack/////////////////////////////////////
+////////////////////////////////////Execution Context and the Execution Stack///////////////////////////////////////////
 
 // all JS codes need an enviroment to run in, and these enviroments are called execution context 
 
-// lastName === window.lastName, because lastName is a property of the window object
+// lastName === window.lastName, because lastName is a property of the window object and all things are part of this window object, so it seems the everything is js is an object
 
 // Everytime  I call a function it gets it's brand new execution context, there seems to be an excution stack when a function is being called within another function. The function that was called last is going to be the execution context block ontop 
 
 
-////////////////////////////////////////////////////////Hoisting//////////////////////////////////////
+////////////////////////////////////////////////////////Hoisting//////////////////////////////////////////////////////
 
-// // Example of Hoisting - wiht function decleration
+// // Example of Hoisting - with function decleration
 // calculateAge(1965) //--> 54
 
 // function calculateAge(year){
 //   console.log(2019 - year)
 // }
  
-// // Hoisting will NOT work with function expression
+// // Hoisting will NOT work with function expression, but it will work with function declerations  
 // retirement(1990) //--> error 
 
 // const retirement = function(year){
@@ -42,7 +42,7 @@
 //   console.log(age)
 // }
 
-// foo()
+// foo() //65
 
 
 /////////////////////////////////////////////////////////Scoping/////////////////////////////////////////////////////
