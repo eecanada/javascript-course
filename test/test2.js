fa3f6a250ae4b19492283de4a34193f36bc9cf72
Eder Canada <canada.eder@gmail.com>
2020-08-25 20:50:51 -0700

    promises then and catch

--- test/test2.js
+++ test/test2.js
@@ -7,11 +7,39 @@ const getIDs = new Promise((resolve,reject)=> {
    }, 1500)
 })
 
+const getRecipe = idFromGetIDs2 => {
+   return new Promise((resolve,reject)=>{
+      setTimeout((idFromGetIDs2)=>{
+         const recipe = {title: 'Fresh Tomato pasta', publisher: 'eder'}
+         resolve(`${idFromGetIDs2}: ${recipe.title}`)
+      },1000,idFromGetIDs2 )
+   })
+} 
+
+const getRelated = publisherFromGetRecipe => {
+   return new Promise((resolve,reject)=>{
+      setTimeout((publisherFromGetRecipe)=>{
+         const recipe2 = {title: 'pizza', publisher: 'eder'}
+         resolve(`Here is another recipe ${recipe2.title} ${publisherFromGetRecipe} made up`)
+      },1000, publisherFromGetRecipe)
+   })
+}
+
 // consumed the successful promise 
 getIDs
    .then(idsFromGetIDs => {
       console.log(idsFromGetIDs)
+      return getRecipe(idsFromGetIDs[2]) 
+   })
+   .then(resolveFromGetRecipe =>{ // this is the resolve 
+      console.log(resolveFromGetRecipe)
+      return getRelated('eder')
+   })
+   .then(resolveFromGetRelated=>{
+      console.log(resolveFromGetRelated)
    })
    .catch(err => {
       console.log('an error has happened')
    })
+
+
