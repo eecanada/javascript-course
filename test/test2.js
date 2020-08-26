717083f077eb81e778ca042c197b83cb9fae31ae
Eder Canada <canada.eder@gmail.com>
2020-08-25 20:05:16 -0700

    reject promise

--- test/test2.js
+++ test/test2.js
@@ -1,13 +1,17 @@
 ////////////////////////////////////////////////////////PROMISES//////////////////////////////////////////////////
 
-
+// created promise with executor function
 const getIDs = new Promise((resolve,reject)=> {
    setTimeout(()=>{
     resolve([523,883,432,974]) // the data that is returned, promise is fufilled
    }, 1500)
 })
 
+// consumed the successful promise 
 getIDs
-   .then(idsFromGetIDs =>{
+   .then(idsFromGetIDs => {
       console.log(idsFromGetIDs)
+   })
+   .catch(err => {
+      console.log('an error has happened')
    })
