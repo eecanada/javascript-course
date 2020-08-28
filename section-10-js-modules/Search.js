f967d66a9bd9b609a4476705d50b1b34a347e335
Eder Canada <canada.eder@gmail.com>
2020-08-27 19:51:15 -0700

    export modules

--- section-10-js-modules/Search.js
+++ section-10-js-modules/Search.js
@@ -1,1 +1,7 @@
-export default 'I am an exported string'
+export default 'I am an exported string';
+
+import {add, multiply, ID} from '../test/SearchView';
+import {add as a, multiply as b, ID as c} from '../test/SearchView'
+import * as searchView from '../test/SearchView'
+
+console.log( )
--- test/SearchView.js
+++ test/SearchView.js
@@ -1,3 +1,13 @@
 import str from '../section-10-js-modules/Search'
 
+
 console.log(str)
+
+
+export const add = ((a,b) => {
+  return a + b
+})
+
+export const multiply = ( a,b) => a * b
+
+export const ID = 24
